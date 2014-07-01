62c4c4c32153b6d0d0e9cdb76f4d79da41bfcb48
Gilles-Philippe Leblanc <gilles-philippe.leblanc@umontreal.ca>
2014-07-01 13:52:59 -0400

    Update for 2.6 version of Moodle, some minor fixes and some code cleanup

--- lang/fr/local_parents.php
+++ lang/fr/local_parents.php
@@ -1,10 +1,14 @@
 <?php
+$string['childdepartment'] = 'École';
+$string['childemail'] = 'Email enfant';
+$string['childfullname'] = 'Nom complet enfant';
+$string['childid'] = 'ID enfant';
+$string['childinstitution'] = 'Nom école';
+$string['childusername'] = 'Code enfant';
+$string['link_name'] = 'Nom du lien';
+$string['link_name_desc'] = 'Lien qui apparaît dans l\'arborescence';
+$string['parents'] = 'Parents';
+$string['parent_role'] = 'Rôle parent';
+$string['parent_role_desc'] = 'Rôle qui représente les parents';
 $string['pluginname'] = 'Liste des parents des participants';
 $string['pluginname_desc'] = 'Affiche la liste de tous les parents des participants d\'un cours';
-$string['parents'] = 'Parents';
-$string['list'] = 'lliisstt';
-$string['link_name'] = 'Nom du lien';
-$string['link_name_desc'] = 'Lien qui apparait dans l\'arborescence';
-$string['parent_role'] = 'Role parent';
-$string['parent_role_desc'] = 'Role qui represente les parents';
-
